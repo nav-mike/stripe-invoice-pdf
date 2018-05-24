2363c86eb45e92fe8c84b9f876dacb11948645c7
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-05-24 11:20:07 +0000

    add routes for invoice controller

--- lib/generators/stripe_invoice_pdf_generator.rb
+++ lib/generators/stripe_invoice_pdf_generator.rb
@@ -4,6 +4,10 @@ if defined?(Rails)
     
     def copy_initializer
       copy_file 'stripe_invoice_pdf.rb', 'config/initializers/stripe_invoice_pdf.rb'
+      
+      inject_into_file 'config/routes.rb', after: 'Rails.application.routes.draw do' do
+          "\n\tget 'invoices/:invoice_id' => 'invoices_controller#show', constraints: {invoice_id: /[a-z0-9\_]+/i}"
+      end
     end
   end
 end

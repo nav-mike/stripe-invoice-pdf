8548ad5e2bac91d4fa5ba76744ac5692305e5302
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-05-25 11:53:34 +0000

    update generator

--- lib/generators/stripe_invoice_pdf_generator.rb
+++ lib/generators/stripe_invoice_pdf_generator.rb
@@ -10,6 +10,15 @@ if defined?(Rails)
       inject_into_file 'config/routes.rb', after: 'Rails.application.routes.draw do' do
         "\n\tget 'invoices/:invoice_id' => 'invoices_controller#show', constraints: {invoice_id: /[a-z0-9\_]+/i}"
       end
+
+      copy_file 'app/controllers/invoices_controller.rb'
+
+      copy_file 'app/views/invoices/show.html.erb'
+
+      copy_file 'app/assets/stylesheets/stripe_invoice_pdf.css'
+      copy_file 'app/assets/stylesheets/bootstrap.min.css'
+
+      copy_file 'app/assets/fonts/Lato-Regular.ttf'
     end
   end
 end

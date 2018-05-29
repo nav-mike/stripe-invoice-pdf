b7386223781c11cfbac74cdee323c49623034768
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-05-29 15:14:38 +0300

    add logo tag

--- lib/stripe_invoice_pdf/helpers.rb
+++ lib/stripe_invoice_pdf/helpers.rb
@@ -2,10 +2,12 @@
 
 class StripeInvoicePdf
   module Helpers
-    def logo_svg(name = nil)
-      return logo_svg_file(name) if name
-      raise 'Insert SVG here!' unless block_given?
-      yield
+    def invoice_logo_tag
+      return yield if block_given?
+      name = StripeInvoicePdf.company_logo
+      exts = %w[.jpg .png .jpeg]
+      return image_logo(name) if exts.include?(File.extname(name))
+      logo_svg name
     end
 
     private
@@ -14,6 +16,16 @@ class StripeInvoicePdf
       path = "#{Rails.root}/app/assets/images/#{name}.svg"
       return File.read(path).html_safe if File.exist?(path)
       '(not found)'
+    end
+
+    def image_logo(name)
+      image_tag name
+    end
+
+    def logo_svg(name = nil)
+      return logo_svg_file(name) if name
+      raise 'Insert SVG here!' unless block_given?
+      yield
     end
   end
 end

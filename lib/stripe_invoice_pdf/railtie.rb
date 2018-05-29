51d4a3e73911d01600851994f8eb03fc7c9c1b35
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-05-29 14:12:42 +0300

    fix railtie

--- lib/stripe_invoice_pdf/railtie.rb
+++ lib/stripe_invoice_pdf/railtie.rb
@@ -5,11 +5,17 @@ require 'stripe_invoice_pdf/helpers'
 class StripeInvoicePdf
   if defined?(Rails.env)
     if Rails::VERSION::MAJOR >= 5
-      initializer 'stripe_invoice_pdf.register' do |_app|
-        ActionView::Base.send :include, StripeInvoicePdf::Helpers
+      class StripeInvoicePdfRailtie < Rails::Railtie
+        initializer 'stripe_invoice_pdf.register' do |_app|
+          ActionView::Base.send :include, StripeInvoicePdf::Helpers
+        end
       end
     elsif Rails::VERSION::MAJOR == 4
-      ActionView::Base.send :include, StripeInvoicePdf::Helpers
+      class StripeInvoicePdfRailtie < Rails::Railtie
+        initializer 'stripe_invoice_pdf.register' do |_app|
+          ActionView::Base.send :include, StripeInvoicePdf::Helpers
+        end
+      end
     end
   end
 end
